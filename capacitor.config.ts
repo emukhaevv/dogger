8943230014475a3909aa294009ef70cb52fc20ea
jumpeebunee <jumpeebunee@gmail.com>
2025-02-01 18:54:52 +0300

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'doggertodoapp.app',
+	appName: 'dogger',
+	webDir: 'build'
+};
+
+export default config;
